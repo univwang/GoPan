3454b90ef55dd0f5cc20a70115b7dab5f3bc4891
univwang <2162768982@qq.com>
2023-02-11 16:02:21 +0800

    0211

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Nav-bar></Nav-bar>
   <router-view/>
 </template>
 
+<script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+import  NavBar from "@/components/NavBar.vue" 
+export default {
+  components: {
+    NavBar,
+  },
+  setup() {
+
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  /* background-image: url("@/assets/img/background.jfif"); */
+  background-size: cover;
 }
 </style>
---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link">Disabled</a>
+  </li>
+</ul>
+</template>
+
+
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
